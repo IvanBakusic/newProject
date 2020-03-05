a0b978f989d1b0c01381c5f4714e09b6283747ad
bakusicivan@gmail.com <bakusicivan@gmail.com>
2020-03-05 15:08:22 +0100

    Adapted background for mobile phones

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,17 +1,25 @@
 import React from "react"
 import "../modules/layout.css"
 import "../../../node_modules/font-awesome/css/font-awesome.css"
+import backgroundImage from "../../images/backgroundNatureNWT.jpg"
 
 import Header from "./header"
 import Footer from "./footer"
 
 const Layout = ({ children }) => {
   return (
-    <div id="wholePage">
-      <Header />
-      {children}
-      <Footer />
-    </div>
+    <>
+      <img
+        src={backgroundImage}
+        id="backgroundImage"
+        alt="backgroundImage.jpg"
+      />
+      <div id="wholePage">
+        <Header />
+        {children}
+        <Footer />
+      </div>
+    </>
   )
 }
 
